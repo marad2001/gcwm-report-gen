280175df749a37dade73ed07e90bec178157d95c
Mark Ashworth <mark.ashworth@gcwm.co.uk>
2024-12-20 10:35:20 +0000

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use lambda_http::{http::{Response, StatusCode}, run, service_fn, Error, IntoResponse, Request, RequestPayloadExt};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+mod domain;
+mod driven;
+mod driving;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    
+    run(service_fn(function_handler)).await
+}
+
+pub async fn function_handler(event: Request) -> Result<impl IntoResponse, Error> {
+    let body = event.payload::<MyPayload>()?;
+
+
+
+
+    
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", "application/json")
+        .body(json!({
+            "message": "Hello World",
+            "payload": body,
+          }).to_string())
+        .map_err(Box::new)?;
+
+    Ok(response)
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct MyPayload {
+    pub prop1: String,
+    pub prop2: String,
+}
