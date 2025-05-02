a0696d06d08a6370c0b687c121edc96a6bccca6a
Mark Ashworth <mark.ashworth@gcwm.co.uk>
2025-05-02 13:34:15 +0100

    updated payload error handling main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use lambda_http::{http::{Response, StatusCode}, run, service_fn, Error, IntoResponse, Request, RequestExt, RequestPayloadExt};
+use driving::data_transfer_object::{self, DataTransferObject};
+use lambda_http::{ext::PayloadError, http::{Response, StatusCode}, run, service_fn, Error, IntoResponse, Request, RequestExt, RequestPayloadExt};
 use serde_json::json;
 use http::Method;
 use dotenv::dotenv;
@@ -23,13 +24,26 @@ pub async fn function_handler(event: Request) -> Result<impl IntoResponse, Error
     let method = event.method();
     let path_parameters = event.path_parameters();
     println!("Path Parameters: {:?}", path_parameters.first("proxy"));
+
+    enum PayloadType {
+        Test(Option<DataTransferObject>),
+        Production(Result<Option<DataTransferObject>, PayloadError>)
+    }
+
     let payload = if path_parameters.first("proxy") == Some("test") {
-        Some(helpers::test_helpers::create_mock_data_transfer_object())
+        PayloadType::Test(Some(helpers::test_helpers::create_mock_data_transfer_object()))
     } else {
-        event.payload::<driving::data_transfer_object::DataTransferObject>()?
+        match event.payload::<driving::data_transfer_object::DataTransferObject>() {
+            Ok(payload) => {
+                PayloadType::Production(Ok(payload))
+            }
+            Err(payload_error) => {
+                PayloadType::Production(Err(payload_error))
+            }
+        }
     };
 
-    println!("Payload: {:?}", payload);
+    //println!("Payload: {:?}", payload);
 
     match method {
         &Method::POST => {
@@ -42,28 +56,68 @@ pub async fn function_handler(event: Request) -> Result<impl IntoResponse, Error
             // } else {
 
                 match payload {
-                    Some(data_transfer_object) => {
+                    PayloadType::Test(test_payload) => {
+                        match test_payload {
+                            Some(data_transfer_object) => {
                         
-                        let report = domain::report::create_report::create_report(data_transfer_object.report_type);
-
-                        let response = Response::builder()
-                            .status(StatusCode::OK)
-                            .header("Content-Type", "application/json")
-                            .body(json!({
-                                "payload": report
-                            }).to_string())
-                            .map_err(Box::new)?;
-
-                        Ok(response)
-
-
+                                let report = domain::report::create_report::create_report(data_transfer_object.report_type);
+        
+                                let response = Response::builder()
+                                    .status(StatusCode::OK)
+                                    .header("Content-Type", "application/json")
+                                    .body(json!({
+                                        "payload": report
+                                    }).to_string())
+                                    .map_err(Box::new)?;
+        
+                                Ok(response)
+        
+        
+                            }
+                            None => {
+                                
+                                let empty_payload_received_response = helpers::response_helpers::message_response("Empty payload request received");
+        
+                                empty_payload_received_response
+        
+                            }
+                        }
                     }
-                    None => {
-                        
-                        let empty_payload_received_response = helpers::response_helpers::message_response("Empty payload request received");
-
-                        empty_payload_received_response
-
+                    PayloadType::Production(payload) => {
+                        match payload {
+                            Ok(data_transfer_object) => {
+                                match data_transfer_object {
+                                    Some(data_transfer_object) => {
+                                        let report = domain::report::create_report::create_report(data_transfer_object.report_type);
+        
+                                        let response = Response::builder()
+                                            .status(StatusCode::OK)
+                                            .header("Content-Type", "application/json")
+                                            .body(json!({
+                                                "payload": report
+                                            }).to_string())
+                                            .map_err(Box::new)?;
+                
+                                        Ok(response)
+                                    }
+                                    None => {
+                                        let empty_payload_received_response = helpers::response_helpers::message_response("Empty payload request received");
+        
+                                        empty_payload_received_response
+                                    }
+                                }
+                            }
+                            Err(error) => {
+                                
+                                let response = Response::builder()
+                                    .status(StatusCode::OK)
+                                    .header("Content-Type", "application/json")
+                                    .body(error.to_string())
+                                    .map_err(Box::new)?;
+            
+                                    Ok(response)
+                            }
+                        }
                     }
                 }
             }
